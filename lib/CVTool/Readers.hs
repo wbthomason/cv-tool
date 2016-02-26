72d92d455d7375598baad3fd6e9313b98e3ce69b
Wil Thomason <wil.thomason@gmail.com>
2016-02-25 22:53:39 -0500

    Readers done modulo testing

--- lib/CVTool/Readers.hs
+++ lib/CVTool/Readers.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE DeriveGeneric #-}
-
 module CVTool.Readers (readJson, readYaml, readToml) where
 
 import Data.Aeson as Ae (eitherDecode', FromJSON(..), toJSON, fromJSON, Result(..))
@@ -9,34 +7,29 @@ import Data.Text (Text)
 import Data.Yaml (decodeEither)
 import GHC.Generics
 import System.Exit
-import Text.Pandoc (readNative, Pandoc(..))
+import Text.Pandoc (readNative, Pandoc(..), Meta(..))
 import Text.Parsec
 import Text.Toml
+import Data.Map.Strict
 
-data CVData = CVData {
-  name :: String
-} deriving (Generic, Show)
 
-instance FromJSON CVData
-
-buildPandoc :: (a -> Either String CVData) -> a -> Pandoc
+buildPandoc :: (a -> Either String Meta) -> a -> Pandoc
 buildPandoc parser rawData = 
   case parser rawData of
-        Right cvData -> Pandoc metaCvData []
+        Right cvData -> Pandoc cvData []
         -- TODO: Actually print exception
         Left errMsg  -> Prelude.head $ Data.Either.rights [readNative "hello"]
-  where metaCvData = Meta fold
 
-tomlToJson :: Text -> Result CVData
+tomlToJson :: Text -> Result Meta
 tomlToJson inputData = 
   case parseTomlDoc "InputCVData" inputData of
         Right toml  -> fromJSON . toJSON $ toml
-        Left err    -> Ae.Error $ "TOML parse error:" ++ (show $ errorPos err)
+        Left err    -> Ae.Error $ "TOML parse error:" ++ show (errorPos err)
 
 eitherDecodeToml inputData = 
   case tomlToJson inputData of
-        Success jsonData             -> Right jsonData
-        Ae.Error err         -> Left $ "TOML parse error:" ++ err
+        Success jsonData     -> Right jsonData
+        Ae.Error err         -> Left err
 
 readJson = buildPandoc eitherDecode'
 readYaml = buildPandoc decodeEither
